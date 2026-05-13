e4760b340bfdabd12d2938c12a35970fc4f5e143
Maarten Hormes <maarten.hormes@gmail.com>
2026-05-13 08:54:00 +0200

    Update shows

--- src/lib/shows.ts
+++ src/lib/shows.ts
@@ -9,12 +9,13 @@ export type Show = {
 };
 
 export const upcomingShows: Show[] = [
-  { date: "10 mei 2026 13u-15u", venue: "Venhof", city: "Herkenbosch", infoUrl: "https://www.instagram.com/p/DXH67I_CGOF/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
   { date: "30 mei 2026 15.30-16.15", venue: "Postert Laef", city: "Posterholt", ticketUrl: "https://roerdalelaef.nl/tickets1/", infoUrl: "https://roerdalelaef.nl" },
+  { date: "20 jun 2026 n.t.b.", venue: "The Movement (Cultuurnacht)", city: "Roermond" },
   { date: "28 jun 2026 n.t.b.", venue: "Gluren bij de Buren 2026", city: "Herten", infoUrl: "https://www.glurenbijdeburen.nl/" },
  ];
 
 export const pastShows: Show[] = [
+  { date: "10 mei 2026", venue: "Venhof", city: "Herkenbosch", infoUrl: "https://www.instagram.com/p/DXH67I_CGOF/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
   { date: "22 mar 2026", venue: "De Boshut", city: "Herkenbosch" },
   { date: "07 nov 2025", venue: "De Spil", city: "Maasbracht" },
   { date: "30 aug 2025", venue: "Verjaardags feest", city: "Herten", private: true },
