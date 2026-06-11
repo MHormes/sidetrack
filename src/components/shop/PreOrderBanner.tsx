b60ebc8bd87141dc212c66d1d50484dd290ba396
Maarten Hormes <maarten.hormes@gmail.com>
2026-06-11 12:26:20 +0200

    pre-order date update

--- src/components/shop/PreOrderBanner.tsx
+++ src/components/shop/PreOrderBanner.tsx
@@ -11,8 +11,9 @@ export default function PreOrderBanner({
       className={`bg-elevated border border-edge py-3 px-4 text-center ${className}`}
     >
       <p className="text-xs sm:text-sm font-bold tracking-wide text-accent max-w-3xl mx-auto">
-        De eerste pre-order sluit op 10-06-2026 om 23.59u. Daarna is bestellen
-        nog mogelijk, maar voldoening van de order wordt niet gegarandeerd.
+        De eerste pre-order is gesloten. Bestellen is nog altijd mogelijk, maar
+        voldoening van de order wordt niet gegarandeerd. Hierover nemen wij
+        contact op.
       </p>
     </div>
   );
