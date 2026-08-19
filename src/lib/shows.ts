dcdc11f44f3eac42978e618a7db01e2c70d97f6b
Maarten Hormes <maarten.hormes@gmail.com>
2026-08-19 15:59:13 +0200

    shows update

--- src/lib/shows.ts
+++ src/lib/shows.ts
@@ -29,7 +29,8 @@ export function stripTime(dateStr: string): string {
 // Add all shows here. upcoming/past split is automatic based on date.
 // Shows become "past" starting the day after the show date.
 export const allShows: Show[] = [
-  { date: "12 sep 2026 n.t.b.", venue: "Scouting", city: "Roermond", private: true },
+  { date: "12 sep 2026", venue: "Scouting", city: "Roermond", private: true },
+  { date: "22 aug 2026", venue: "Fuckin Band in de Tuin feestje", city: "Herkenbosch", private: true },
   { date: "29 jul 2026 18.00 & 19.00", venue: "Sjoemelmert Remunj (Aeve Choppe)", city: "Roermond"},
   { date: "28 jun 2026 13.45, 15.15 & 16.45", venue: "Gluren bij de Buren 2026", city: "Herten", infoUrl: "https://glurenbijdeburen.nl/nl/programma/roermond#1416" },
   { date: "20 jun 2026", venue: "Munsterplein (Cultuurnacht)", city: "Roermond", infoUrl: "https://www.instagram.com/p/DZLE8lYIJQR/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" },
